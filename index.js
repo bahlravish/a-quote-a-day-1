fbce9dbe30c0a93492fbaaaa55a438b37cfdf5ed
{ravish} <{bahlravish@gmail.com}>
2018-06-15 22:48:24 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/hello',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+var PORT = process.env.PORT || 8080;
+
+var server = app.listen(PORT,function(req,res){
+    console.log("Server Started!");
+});
+
+
+
+
+
